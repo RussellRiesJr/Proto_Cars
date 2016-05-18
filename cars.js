5dcf16ce5835fb95dba9176635e8b242e0db2142
Russell Ries <riesru@gmail.com>
2016-05-18 15:13:59 -0500

    finish exercise

--- cars.js
+++ cars.js
@@ -1,41 +1,66 @@
+
+var richGuyCars = [];
+
 /*
   Create a function named Car with one property named `manufactured_date`
+  whose value will be Date.now()
  */
+function Car () {
+  // Create the manufactured date property (see above)
+  this.manufactured_date = Date.now();
+}
 
 /*
-  Create a function named Make.
-  Set its prototype to the Car function.
-  Define a property `manufacaturer` with a default value of empty string.
+  Create a constructor function named Make with one
+  argument that will hold the manufacturer of the Make (see below).
+
+  Set its prototype to a new instance of the Car function.
+
+  Define a property `manufacturer` and assign the
+  manufacturer property to the argument value.
  */
-
-
+function Make (maker) {
+  this.manufacturer = maker;
+}
+// Set the prototype to Car
+Make.prototype = new Car();
+/*
+  Create a new constructor function for the make of your favorite car.
+  It will inherit from the general Make function.
+ */
+function Toyota (name) {
+  this.modelName = name;
+}
+Toyota.prototype = new Make("Toyota");
 
 /*
-    Create a function named Model.
-    Set its prototype to the Make function.
-    Define a property `modelName` with a default value of empty string.
+    Create a constructor function named after your
+    favorite car model with one argument that will
+    hold the specific name of the model (see example below).
+
+    Set its prototype to a new instance of the make you defined
+    above and pass in the string value of the model.
+
+    Define a property to hold the name of the model and set its
+    value to the argument value.
+
 */
-
-
-/*
-    Create a new Make object - your favorite one (e.g. Mini)
-*/
-
-
-/*
-    Create a new Model object of your favorite model (e.g. Clubman)
-*/
-
+function Camry (num) {
+  this.license = num
+}
+// Set the prototype to appropriate model you created above and set the model name argument
+Camry.prototype = new Toyota("Camry");
 /*
     Being an avid car collector, you own three different cars all of
     same model from your favorite manufacturer.
 
-    Create three new objects for each of your three favorite cars.
-*/
+    Create three new instances for each of your three favorite cars.
 
-
-/*
-    Being a rich, avid car collector, you own 2 cars from your second
-    favorite manufacturer. You also one 1 car from your third favorite
-    manufacturer.
+    Create a new property on each object to hold the license plate
+    number for each car.
 */
+var camry1 = new Camry("345 ABC");
+var camry2 = new Camry("813 BHS");
+var camry3 = new Camry("924 NPD");
+richGuyCars.push(camry1, camry2, camry3);
+console.log("cars", richGuyCars)
